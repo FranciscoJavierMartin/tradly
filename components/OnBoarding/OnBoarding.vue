ee7f3ebade267ed8313e63af4791b61c036d1248
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2022-05-01 13:05:24 +0200

    Add item wrapper

--- components/OnBoarding/OnBoarding.vue
+++ components/OnBoarding/OnBoarding.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="c-on-boarding">
-    <img src="../../assets/imgs/on-boarding-step-1.svg" />
-    <h3>Empowering Artisans, Farmers & Micro Business</h3>
-    <div class="steps">
-      <div class="active"></div>
-      <div></div>
-      <div></div>
+    <div class="item">
+      <img src="../../assets/imgs/on-boarding-step-1.svg" />
+      <h3>Empowering Artisans, Farmers & Micro Business</h3>
+      <div class="steps">
+        <div class="active"></div>
+        <div></div>
+        <div></div>
+      </div>
+      <button>Next</button>
     </div>
-    <button>Next</button>
   </div>
 </template>
 
@@ -19,62 +21,70 @@
 @import '../../assets/styles/main.scss';
 
 .c-on-boarding {
+  background: linear-gradient($dark-green 50%, $white 50%);
   display: flex;
   flex-direction: column;
   height: 100vh;
-  width: 100vw;
-  background: linear-gradient($dark-green 50%, $white 50%);
   justify-content: flex-end;
-  align-items: center;
+  width: 100vw;
 
-  img {
-    background-color: $white;
-    padding: 40px 5px 5px;
-    border-radius: 8px;
-    width: 70%;
-  }
-
-  h3 {
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 30px;
-    text-align: center;
-    letter-spacing: -0.482353px;
-    color: $dark-green;
-  }
-
-  .steps {
+  .item {
+    align-items: center;
     display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 15px;
+    row-gap: 15px;
 
-    div {
-      margin: 5px;
-      width: 12px;
-      height: 12px;
-      background: $light-green;
-      border-radius: 50%;
-      opacity: 0.6;
-      mix-blend-mode: normal;
+    img {
+      background-color: $white;
+      border-radius: 8px;
+      padding: 40px 5px 5px;
+      width: 70%;
+    }
 
-      &.active {
-        background-color: $dark-green;
-        opacity: 1;
+    h3 {
+      color: $dark-green;
+      font-size: 20px;
+      font-weight: 500;
+      letter-spacing: -0.482353px;
+      line-height: 30px;
+      text-align: center;
+    }
+
+    .steps {
+      display: flex;
+
+      div {
+        background: $light-green;
+        border-radius: 50%;
+        height: 12px;
+        margin: 5px;
+        mix-blend-mode: normal;
+        opacity: 0.6;
+        width: 12px;
+
+        &.active {
+          background-color: $dark-green;
+          opacity: 1;
+        }
       }
     }
-  }
 
-  button {
-    font-family: $monserrat-font;
-    background-color: $dark-green;
-    border-radius: 24px;
-    color: $white;
-    text-align: center;
-    width: 100%;
-    font-size: 18px;
-    font-weight: 600;
-    line-height: 18px;
-    text-decoration: none;
-    border: none;
-    padding: 15px;
+    button {
+      background-color: $dark-green;
+      border-radius: 24px;
+      border: none;
+      color: $white;
+      font-family: $monserrat-font;
+      font-size: 18px;
+      font-weight: 600;
+      line-height: 18px;
+      padding: 15px;
+      text-align: center;
+      text-decoration: none;
+      width: 100%;
+    }
   }
 }
 </style>
